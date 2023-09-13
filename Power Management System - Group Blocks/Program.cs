56e22dcc283176882f513a632d49fb71027f4ad7
Pete-sSake <poncejorge90@gmail.com>
2023-09-12 22:57:04 -0400

    from other laptop

--- Power Management System - Group Blocks/Program.cs
+++ Power Management System - Group Blocks/Program.cs
@@ -31,24 +31,23 @@ namespace IngameScript
 
         /*REQUIRED SETUP:
         */
-
         //Input the Batteries' Group Name in between the quotation marks ("")
-        public string batteriesGroupName = "Dover Base - Batteries - Power";
+        public const string batteriesGroupName = "Dover Base - Batteries - Power";
         //Input the Hydrogen Engines' Group Name in between the quotation marks ("")
-        public string hydrogenEnginesGroupName = "Dover Base - Hydrogen Engines - Power";
+        public const string hydrogenEnginesGroupName = "Dover Base - Hydrogen Engines - Power";
         //Input the Hydrogen Reactors' Group Name in between the quotation marks ("")
-        public string reactorsGroupName = "Dover Base - Reactors - Power";
+        public const string reactorsGroupName = "Dover Base - Reactors - Power";
 
         /*OPTIONAL SETUP
         */
         //Replace the "0.20" with the decimal value of the percentage at which the Hydrogen Engines will turn ON
-        public float hydrogenEnginesToggleMinThreshold = 0.20f;
+        public const float hydrogenEnginesToggleMinThreshold = 0.20f;
         //Replace the "0.35" with the decimal value of the percentage at which the Hydrogen Engines will turn OFF
-        public float hydrogenEnginesToggleMaxThreshold = 0.35f;
+        public const float hydrogenEnginesToggleMaxThreshold = 0.35f;
         //Replace the "0.15" with the decimal value of the percentage at which the Reactors will turn ON
-        public float reactorsToggleMinThreshold = 0.15f;
+        public const float reactorsToggleMinThreshold = 0.15f;
         //Replace the "0.25" with the decimal value of the percentage at which the Reactors will turn OFF
-        public float reactorsToggleMaxThreshold = 0.25f;
+        public const float reactorsToggleMaxThreshold = 0.25f;
 
         /*DO NOT MODIFY BELOW THIS LINE
         //
@@ -71,114 +70,61 @@ namespace IngameScript
             }
 
             //Detect Hydrogen Engines
-            bool hydrogenEnginesExist = true;
             List<IMyPowerProducer> hydrogenEngines = powerProducerListGenerator(hydrogenEnginesGroupName);
-            if (hydrogenEngines.Count == 0)
-            {
-                hydrogenEnginesExist = false;
-                Echo("No Hydrogen Engines Detected");
-            }
-
             //Detect Reactors
-            bool reactorsExist = true;
             List<IMyPowerProducer> reactors = powerProducerListGenerator(reactorsGroupName);
-            if (reactors.Count == 0)
-            {
-                reactorsExist = false;
-                Echo("No Hydrogen Engines Detected");
-            }
 
             //Ends Iteration If No Hydrogen Engines or Reactors are Detected
-            if (!hydrogenEnginesExist && !reactorsExist)
+            if (hydrogenEngines.Count == 0 && reactors.Count == 0)
             {
-                Echo("No Hydrogen Engines or Reactors are Detected");
+                Echo("No Hydrogen Engines or Reactors were Detected");
                 return;
             }
 
             //Prints Average Charge Level of Batteries
             float chargeLevel = chargeLevelCalculator(batteries);
             double charge = Math.Round(chargeLevel * 100, 2);
-            Echo("Battery Charge:   " +  charge + "%");
+            Echo($"Battery Charge________{charge}%");
 
-            //Turns Hydrogen Engines and Reactors ON or OFF based on batteries Charge Level
-            powerProducersToggle(chargeLevel, hydrogenEnginesExist, reactorsExist, hydrogenEngines, reactors);
-
+            if (hydrogenEngines.Count == 0)
+            {
+                Echo("No Hydrogen Engines Detected");
+            } else
+            {
+                handlePowerProducers(chargeLevel, hydrogenEngines, hydrogenEnginesToggleMinThreshold,
+                    hydrogenEnginesToggleMaxThreshold, "Hydrogen Engines_____");
+            }
+            
+            if (reactors.Count == 0)
+            {
+                Echo("No Hydrogen Engines Detected");
+            } else
+            {
+                handlePowerProducers(chargeLevel, reactors, reactorsToggleMinThreshold,
+                    reactorsToggleMaxThreshold, "Reactors_____________");
+            }
         }//End of Main method
 
-        //Turns Hydrogen Engines and Reactors ON or OFF base on batteries Charge Level
-        public void powerProducersToggle(float chargeLevel, bool hydrogenEnginesExist, bool reactorsExist,
-                                         List<IMyPowerProducer> hydrogenEngines, List<IMyPowerProducer> reactors)
+        private void handlePowerProducers(float chargeLevel, List<IMyPowerProducer> powerProducers, float toggleMinThreshold,
+                                          float toggleMaxThreshold, string messagePrefix)
         {
-            if (hydrogenEnginesExist)
+            if (chargeLevel < toggleMinThreshold)
             {
-                if (chargeLevel < hydrogenEnginesToggleMinThreshold)
+                foreach (IMyPowerProducer block in powerProducers)
                 {
-                    foreach (IMyPowerProducer block in hydrogenEngines)
-                    {
-                        block.Enabled = true;
-                    }
-                }
-                else if (chargeLevel >= hydrogenEnginesToggleMaxThreshold)
-                {
-                    foreach (IMyPowerProducer block in hydrogenEngines)
-                    {
-                        block.Enabled = false;
-                    }
-                }
-                {
-                    bool enabled = false;
-                    foreach (IMyPowerProducer block in hydrogenEngines)
-                    {
-                        if (block.Enabled == true)
-                        {
-                            enabled = true;
-                        }
-                    }
-                    if (enabled)
-                    {
-                        Echo("Hydrogen Engines:   ON");
-                    }
-                    else
-                    {
-                        Echo("Hydrogen Engines:   OFF");
-                    }
+                    block.Enabled = true;
                 }
             }
-            if (reactorsExist)
+            else if (chargeLevel >= toggleMaxThreshold)
             {
-                if (chargeLevel < reactorsToggleMinThreshold)
+                foreach (IMyPowerProducer block in powerProducers)
                 {
-                    foreach (IMyPowerProducer block in reactors)
-                    {
-                        block.Enabled = true;
-                    }
-                }
-                else if (chargeLevel >= reactorsToggleMaxThreshold)
-                {
-                    foreach (IMyPowerProducer block in reactors)
-                    {
-                        block.Enabled = false;
-                    }
-                }
-                {
-                    bool enabled = false;
-                    foreach (IMyPowerProducer block in reactors)
-                    {
-                        if (block.Enabled == true)
-                        {
-                            enabled = true;
-                        }
-                    }
-                    if (enabled)
-                    {
-                        Echo("Reactors:   ON");
-                    }
-                    else
-                    {
-                        Echo("Reactors:   OFF");
-                    }
+                    block.Enabled = false;
                 }
             }
+
+            bool enabled = powerProducers.Any(p => p.Enabled);
+            Echo($"{messagePrefix}" + (enabled ? "ON" : "OFF"));
         }
 
         //Calculates Charge Level of all functional batteries as Current Charge divided by Max Charge
